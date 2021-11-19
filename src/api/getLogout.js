50f6415075b3d1aad3c24bdc47827035eb203f10
TranCongNguyen <congnguyen.230901@gmail.com>
2021-11-19 16:25:31 +0700

    Create Logout.js

--- src/api/getLogout.js
+++ src/api/getLogout.js
@@ -3,10 +3,11 @@ import axios from "axios";
 export const getLogout = async (formData) => {
     let url =
         process.env.REACT_APP_ENV.trim() === "dev"
-            ? process.env.REACT_APP_ENV_DEV_URL
-            : process.env.REACT_APP_ENV_PROD_URL;
+            ? process.env.REACT_APP_DEV_URL
+            : process.env.REACT_APP_PROD_URL;
 
     url += "/logout";
+    console.log(url)
     let data;
     let respone = await axios
         .post(url,{ 
