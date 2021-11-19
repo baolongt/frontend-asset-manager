fc76f4002b6da5a204fee77000737147748940a7
TranCongNguyen <congnguyen.230901@gmail.com>
2021-11-19 09:22:53 +0700

    update App UI

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { getLogin } from "./api/getLogin";
 import ChangePass from "./components/ChangePassword";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import "./App.css"
 import Home from "./Home/Home";
 function App() {
 	return (
-		<div className="App">
+		<div>
 			<Route path="/login" component={Login} />
 			<Route path="/register" component={Register} />
 			<Route path="/authenticate/changePass" component={ChangePass}/>
